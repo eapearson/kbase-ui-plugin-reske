210d128756426ef4c49c0b9a82f149290905ce33
Erik Pearson <eapearson@lbl.gov>
2017-09-13 20:02:43 -0700

    minor ui updates [TASK-1051]

--- src/plugin/modules/v2/narrative/components/controls.js
+++ src/plugin/modules/v2/narrative/components/controls.js
@@ -81,7 +81,7 @@ define([
                         textInput: 'search.searchInput',
                         hasFocus: true
                     },
-                    placeholder: 'Search KBase Data with RESKE Search!',
+                    placeholder: 'Search KBase Narratives with RESKE Search!',
                     class: 'form-control'
                 }),
                 div({
